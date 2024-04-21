38efa32cca982ec79ff9854287ee4a5d6266565f
laurl <laurlumezeanu@gmail.com>
2024-04-21 16:08:00 +0300

    first commit

---
+++ src/Homework.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        Person person1 = new Person("Laur", 46);
+        Person person2 = new Person("Nicolae", 6);
+        System.out.println(person1.getName() + " is " + person1.getAge() + " years old.");
+        System.out.println(person2.getName() + " is " + person2.getAge() + " years old.\n");
+    }
+}
+import java.time.LocalDate;
+import java.time.Period;
+
+        class JavaLocalDate {
+
+            public static void main(String[] args) {
+
+                LocalDate from = LocalDate.of(2020, 4, 20);
+                LocalDate to = LocalDate.of(2020, 1, 1);
+
+                Period period = Period.between(from, to);
+
+                System.out.print(period.getYears() + " years,");
+                System.out.print(period.getMonths() + " months,");
+                System.out.print(period.getDays() + " days");
+
+            }
+        }
+    }
+}
